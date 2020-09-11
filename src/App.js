702cff4cc01fb7f2149c2f9ac0606f584ca745b8
zuizihad <ahmedxihad@gmail.com>
2020-09-11 19:12:21 +0600

    basic implementation of directory & menu item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import HomePage from './pages/homepage/homepage.component';
 function App() {
   return (
     <div>
-      hello
+      <HomePage/>
     </div>
   );
 }
