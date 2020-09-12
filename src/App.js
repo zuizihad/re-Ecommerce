bd31652c61c35569a7787e3eeb7d729e62ab6669
zuizihad <ahmedxihad@gmail.com>
2020-09-12 14:02:02 +0600

    shop collection preview component implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import {BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
+
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Router>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Router>
     </div>
   );
 }
