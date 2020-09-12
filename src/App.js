b038a207a1b27c8ed41d4d5c9215db90db9d2e86
zuizihad <ahmedxihad@gmail.com>
2020-09-13 01:17:08 +0600

    header component created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import {BrowserRouter as Router, Route } from 'react-router-dom'
+import {BrowserRouter, BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'
-
+import Header from './components/header/header.component'
 function App() {
   return (
     <div>
+    <BrowserRouter>
+    <Header/>
       <Router>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
       </Router>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import './shop.styles.scss'
 import SHOP_DATA from './shop.data'
 import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+
 class ShopPage extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             collections: SHOP_DATA
         }
