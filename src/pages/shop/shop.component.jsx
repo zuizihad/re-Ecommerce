52d22a2e61942bd3a8bec8426672eba81d5b44fe
zuizihad <ahmedxihad@gmail.com>
2020-09-12 14:30:44 +0600

    collection preview and collection item component implemented

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './shop.styles.scss'
 import SHOP_DATA from './shop.data'
-import CollectionPreview from '../../components/preview-collection/collection-preview.component'
+import CollectionPreview from '../../components/collection-preview/collection-preview.component'
 class ShopPage extends React.Component {
     constructor(props) {
         super(props)
