9f966d85233f80d0bbd3d2f7056d1d784af58997
Zihadul Islam <ahmedxihad@gmail.com>
2020-10-13 00:25:47 +0600

    signin with email & password success

--- src/components/sign-in/sign-in.components.jsx
+++ src/components/sign-in/sign-in.components.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './sign-in.styles.scss'
 import FormInput from '../../components/fomr-input/form-input.components'
 import CustomButton from '../custom-button/custom-button.components'
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -13,9 +13,17 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({ email: '', password: '' })
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: '', password: '' }); 
+        }catch(error) {
+            console.error(error);
+        }
+       
     }
     handleChange = event => {
         const { value ,name } = event.target;
