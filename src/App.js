60d7b89b5db229a749cd32b0140e65f4d262d6e8
Zihadul Islam <ahmedxihad@gmail.com>
2020-10-12 15:35:42 +0600

    signin component created & custom form-input and custom buttonimplemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import {BrowserRouter, BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import {BrowserRouter, Route, Switch } from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.components';
+
 function App() {
   return (
     <div>
@@ -11,6 +13,7 @@ function App() {
     <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
         <Route path='/shop' component={ShopPage} />
       </Switch>
     </BrowserRouter>
